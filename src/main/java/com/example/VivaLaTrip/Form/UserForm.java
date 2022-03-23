ab1b825f9c7840786645afc89a8041c43f8e80d2
yy__.ss <yyss9103@gmail.com>
2022-03-23 14:32:52 +0900

    롬복 오류 해결, 비밀번호 확인, 회원가입 인풋값 readonly활용

--- src/main/java/com/example/VivaLaTrip/Form/UserForm.java
+++ src/main/java/com/example/VivaLaTrip/Form/UserForm.java
@@ -1,8 +1,6 @@
 package com.example.VivaLaTrip.Form;
 
-import lombok.AllArgsConstructor;
-import lombok.NoArgsConstructor;
-import lombok.ToString;
+import lombok.*;
 
 import javax.validation.constraints.NotBlank;
 import javax.validation.constraints.Pattern;
@@ -10,7 +8,7 @@ import javax.validation.constraints.Size;
 
 @AllArgsConstructor
 @NoArgsConstructor
-@ToString
+@Getter
 public class UserForm {
     @Pattern(regexp = "(?=.*[0-9])(?=.*[a-zA-Z]).{6,20}", message = "아이디는 6~20자 영문 대 소문자, 숫자를 사용하세요.")
     private String username;
@@ -21,38 +19,38 @@ public class UserForm {
 
     @NotBlank(message = "이름은 필수 입력 값입니다.")
     @Pattern(regexp = "^[ㄱ-ㅎ가-힣a-z0-9-_]{2,10}$", message = "이름은 특수문자를 제외한 2~10자리여야 합니다.")
-    private String UserName;
+    private String UserName_;
     private String liked;
 
-    public String getUsername() {
-        return username;
-    }
-
-    public void setUsername(String username) {
-        this.username = username;
-    }
-
-    public String getPassword() {
-        return password;
-    }
-
-    public void setPassword(String password) {
-        this.password = password;
-    }
-
-    public String getUserName() {
-        return UserName;
-    }
-
-    public void setUserName(String userName) {
-        UserName = userName;
-    }
-
-    public String getLiked() {
-        return liked;
-    }
-
-    public void setLiked(String liked) {
-        this.liked = liked;
-    }
+//    public String getUsername() {
+//        return username;
+//    }
+//
+//    public void setUsername(String username) {
+//        this.username = username;
+//    }
+//
+//    public String getPassword() {
+//        return password;
+//    }
+//
+//    public void setPassword(String password) {
+//        this.password = password;
+//    }
+//
+//    public String getUserName() {
+//        return UserName;
+//    }
+//
+//    public void setUserName(String userName) {
+//        UserName = userName;
+//    }
+//
+//    public String getLiked() {
+//        return liked;
+//    }
+//
+//    public void setLiked(String liked) {
+//        this.liked = liked;
+//    }
 }
