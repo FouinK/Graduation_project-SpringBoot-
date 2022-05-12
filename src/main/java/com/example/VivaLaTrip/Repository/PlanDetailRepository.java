bcb94d9024c3bcc066e48c2a360244cdc152d6c9
thsl <soensuckun@naver.com>
2022-05-12 19:14:46 +0900

    plan detail 제작중

--- src/main/java/com/example/VivaLaTrip/Repository/PlanDetailRepository.java
+++ src/main/java/com/example/VivaLaTrip/Repository/PlanDetailRepository.java
@@ -1,4 +1,16 @@
 package com.example.VivaLaTrip.Repository;
 
-public interface PlanDetailRepository {
+import com.example.VivaLaTrip.Entity.Plan;
+import com.example.VivaLaTrip.Entity.PlanDetail;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.List;
+
+public interface PlanDetailRepository extends JpaRepository<PlanDetail, Long> {
+
+
+
+    PlanDetail save(PlanDetail planDetail);
 }
+
