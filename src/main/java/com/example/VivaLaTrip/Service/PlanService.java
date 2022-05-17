03f8420209c3bc57d967cb1c471fbf92f7e8046b
taeuk <971021q@naver.com>
2022-05-17 18:33:33 +0900

    Plan 생성시 사용자 선택으로 Public Plan 생성 고려

--- src/main/java/com/example/VivaLaTrip/Service/PlanService.java
+++ src/main/java/com/example/VivaLaTrip/Service/PlanService.java
@@ -5,7 +5,6 @@ import com.example.VivaLaTrip.Form.*;
 import com.example.VivaLaTrip.Repository.*;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
-import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.security.core.userdetails.User;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
@@ -22,32 +21,28 @@ import java.util.Optional;
 public class PlanService {
     private final PublicPlanRepository publicPlanRepository;
     private final PlanRepository planRepository;
-    private final LikedRepository likedRepository;
     private final UserRepository userRepository;
     private final PlanDetailRepository planDetailRepository;
 
+    PublicPlanService publicPlanService;
 
-
-    public void setPlan_list(PlanSaveDTO map, User user, List<PlaceComputeDTO> computeDTO) {
+    public void setPlan_list(PlanSaveDTO request, User user, List<PlaceComputeDTO> computeDTO) {
         Plan plan = new Plan();
         Optional<UserInfo> userInfo = userRepository.findByID(user.getUsername());
         //로그인 한 user객체에서 userId(1,2,3,...)값 가져와서 리포지토리 아이디 찾는 메소드 호출
 
-        //planRequestDto.toEntity().setUserInfo(planRequestDto.toEntity().getUserInfo());
-        //PlanRequestDto planRequestDto = new PlanRequestDto();
-
-
-
         plan.setUserInfo(userInfo.get());
-        plan.set_public(map.is_public());
-        plan.setTotal_count(map.getCheckedPlace().size());
-        plan.setStart_date(map.getStart_date());
-        plan.setEnd_date(map.getEnd_date());
+        plan.set_public(false);
+        plan.setTotal_count(request.getCheckedPlace().size());
+        plan.setStart_date(request.getStart_date());
+        plan.setEnd_date(request.getEnd_date());
         //plan 객체에 필요한 값들 설정
 
-        //log.info("user id" + userInfo);
-
         planRepository.save(plan);//메소드 이용하여 저장
+
+        if(request.is_public()){
+            publicPlanService.toPublic(plan.getPlanId(), request.getTitle(), user);
+        }
 
         savePlanDetail(computeDTO, user, plan);
     }
@@ -102,7 +97,7 @@ public class PlanService {
 
     public void savePlanDetail(List<PlaceComputeDTO> map, User user, Plan plan){
 
-        List<PlanDetailDTO> planDetailDTO = new ArrayList<>();  //임시 생성-나중에 매개변수로 받을거
+        List<PlanDetailDTO> planDetailDTO = new ArrayList<>();
 
         for (PlaceComputeDTO computeDTO : map){
             PlanDetailDTO planDetailItem = PlanDetailDTO.builder()
@@ -115,11 +110,9 @@ public class PlanService {
         int size = planDetailDTO.size();   //마지막 장소 저장을 위해 크기 구하기
         int dayIndex = 1;
         String placeIdsOfDay = "";    //place_id 문자열
-             //매개변수로 받은 plan이 plan_id 가지고있는지 테스트해야함
         PlanDetail planDetail = new PlanDetail();
+
         for (int i=0;i<size;i++){
-
-
             if (planDetailDTO.get(i).getDay() != dayIndex){  //day가 올라가면
                 planDetail.setPlan(plan);
                 planDetail.setPlace_id(placeIdsOfDay);
@@ -129,8 +122,6 @@ public class PlanService {
                 placeIdsOfDay = "";
                 dayIndex++;
             }
-            //문자열 뒤에 붙이기
-            //문자열 마지막에 ,가 들어가는데 지워야하나-보류
             placeIdsOfDay+=planDetailDTO.get(i).getId()+",";
 
             if (i+1==size){  //마지막 인덱스
