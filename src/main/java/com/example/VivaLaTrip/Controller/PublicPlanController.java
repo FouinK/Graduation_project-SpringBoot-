3c13672a2a701821ae046e328ed342ac90d48a98
Fouink <yyss9103@gmail.com>
2022-05-20 01:12:26 +0900

    좋아요 기능, 내 일정으로 가져오기 기능 매핑 우선 작업 (프론트와 송수신 데이터 결정 필요)

--- src/main/java/com/example/VivaLaTrip/Controller/PublicPlanController.java
+++ src/main/java/com/example/VivaLaTrip/Controller/PublicPlanController.java
@@ -13,6 +13,10 @@ import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.security.core.userdetails.User;
 import org.springframework.web.bind.annotation.*;
 
+import javax.servlet.http.HttpSession;
+import java.util.HashMap;
+import java.util.Map;
+
 @Slf4j
 @RestController
 public class PublicPlanController {
@@ -46,6 +50,63 @@ public class PublicPlanController {
         return ResponseEntity.ok(response);
     }
 
+    @GetMapping("/api/publicPlan/liked")
+    public @ResponseBody
+    ResponseEntity<?> likePublicPlan(@AuthenticationPrincipal User user,
+                                     @CookieValue(name = "JSESSIONID", required = false) String JSESSIONID,
+                                     HttpSession httpSession) {
+
+        Map<String, Object> map = new HashMap<>();
+        //로그인 확인 결과를 담을 Map
+
+        if (!httpSession.getId().equals(JSESSIONID) || user == null) {
+            //로그인 되어있지 않다면
+            log.info("프론트 부터 받아온 세션 값: " + JSESSIONID);
+            log.info("서버 세션 값: " + httpSession.getId());
+            map.put("result","login");
+            //value = login 리턴
+            return ResponseEntity.ok(map);
+        }
+
+        if (true) {         //좋아요를 이미 눌렀다면
+            map.put("result", "overlap");
+            //value = overlap 리턴
+            return ResponseEntity.ok(map);
+        }
+
+//        log.info("입력받은 일정 ID : "+plan_id.toString());
+//        publicPlanService.addLike(plan_id, like, user);
+        //일정번호랑 유저를 입력 받아야함
+
+        return ResponseEntity.ok(map);
+    }
+
+    @GetMapping("/api/publicPlan/copy")
+    public @ResponseBody
+    ResponseEntity<?> copyPublicPlan(@AuthenticationPrincipal User user,
+                                     @CookieValue(name = "JSESSIONID", required = false) String JSESSIONID,
+                                     HttpSession httpSession) {
+
+        Map<String, Object> map = new HashMap<>();
+        //로그인 확인 결과를 담을 Map
+
+        if (!httpSession.getId().equals(JSESSIONID) || user == null) {
+            //로그인 되어있지 않다면
+            log.info("프론트 부터 받아온 세션 값: " + JSESSIONID);
+            log.info("서버 세션 값: " + httpSession.getId());
+            map.put("result","login");
+            //value = login 리턴
+            return ResponseEntity.ok(map);
+        }
+
+        if (true) {         //이미 가져온 일정이라면
+            map.put("result", "overlap");
+            //value = overlap 리턴
+            return ResponseEntity.ok(map);
+        }
+
+        return ResponseEntity.ok(map);
+    }
 
     @PostMapping("/get_plan")
     public String getPlanById(@RequestParam("id") Long plan_id) {
