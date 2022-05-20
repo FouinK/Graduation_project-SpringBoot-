38b4f2ea0a880c68eb98ace70bc3e18c362eadbb
dbstjdgus <dbstj@192.168.219.100>
2022-05-20 18:48:10 +0900

    내일정으로 가져오기 기능 리액트와 합병 완료

--- src/main/java/com/example/VivaLaTrip/Repository/PlanRepository.java
+++ src/main/java/com/example/VivaLaTrip/Repository/PlanRepository.java
@@ -1,6 +1,7 @@
 package com.example.VivaLaTrip.Repository;
 
 import com.example.VivaLaTrip.Entity.Plan;
+import com.example.VivaLaTrip.Entity.PlanDetail;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
@@ -10,6 +11,10 @@ import java.util.List;
 public interface PlanRepository extends JpaRepository<Plan, Long> {
 
     Page<Plan> findByUserInfo_UserId(Long UserId, Pageable pageable);
+
+    Plan findByUserInfo_UserId(Long UserId);
+
+    boolean existsByFromPlanIdAndUserInfo_UserId(Long planId,Long userId);
 
     Plan findByPlanId(Long plan_id);
 
