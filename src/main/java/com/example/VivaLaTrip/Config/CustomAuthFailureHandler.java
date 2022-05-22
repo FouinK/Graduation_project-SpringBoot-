01a98b82a3a41f7a7378eb5ec0cafe30d13be82e
yy__.ss <yyss9103@gmail.com>
2022-05-22 16:06:14 +0900

    로그인 실패 처리

--- src/main/java/com/example/VivaLaTrip/Config/CustomAuthFailureHandler.java
+++ src/main/java/com/example/VivaLaTrip/Config/CustomAuthFailureHandler.java
@@ -18,21 +18,21 @@ import java.net.URLEncoder;
 public class CustomAuthFailureHandler extends SimpleUrlAuthenticationFailureHandler {
     @Override
     public void onAuthenticationFailure(HttpServletRequest request, HttpServletResponse response, AuthenticationException exception) throws IOException, ServletException {
-        String errorMessage;
+//        String errorMessage;
 
-        if (exception instanceof BadCredentialsException) {
-            errorMessage = "아이디 또는 비밀번호가 맞지 않습니다. 다시 확인해 주세요.";
-        } else if (exception instanceof InternalAuthenticationServiceException) {
-            errorMessage = "계정이 존재하지 않습니다. 회원가입 진행 후 로그인 해주세요.";
-        } else if (exception instanceof UsernameNotFoundException) {
-            errorMessage = "계정이 존재하지 않습니다. 회원가입 진행 후 로그인 해주세요.";
-        } else if (exception instanceof AuthenticationCredentialsNotFoundException) {
-            errorMessage = "인증 요청이 거부되었습니다. 관리자에게 문의하세요.";
-        } else {
-            errorMessage = "알 수 없는 이유로 로그인에 실패하였습니다 관리자에게 문의하세요.";
-        }
-        errorMessage = URLEncoder.encode(errorMessage, "UTF-8");
-        setDefaultFailureUrl("/login?error=true&exception="+errorMessage);
+//        if (exception instanceof BadCredentialsException) {
+//            errorMessage = "아이디 또는 비밀번호가 맞지 않습니다. 다시 확인해 주세요.";
+//        } else if (exception instanceof InternalAuthenticationServiceException) {
+//            errorMessage = "계정이 존재하지 않습니다. 회원가입 진행 후 로그인 해주세요.";
+//        } else if (exception instanceof UsernameNotFoundException) {
+//            errorMessage = "계정이 존재하지 않습니다. 회원가입 진행 후 로그인 해주세요.";
+//        } else if (exception instanceof AuthenticationCredentialsNotFoundException) {
+//            errorMessage = "인증 요청이 거부되었습니다. 관리자에게 문의하세요.";
+//        } else {
+//            errorMessage = "알 수 없는 이유로 로그인에 실패하였습니다 관리자에게 문의하세요.";
+//        }
+//        errorMessage = URLEncoder.encode(errorMessage, "UTF-8");
+        setDefaultFailureUrl("/loginFail");
         super.onAuthenticationFailure(request, response, exception);
     }
 }
