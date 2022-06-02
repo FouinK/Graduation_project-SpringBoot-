62f1c1381bd68c661f2caa5d7e61eb922fe752b3
yy__.ss <yyss9103@gmail.com>
2022-06-02 13:44:33 +0900

    PublicPlan Table의 comment속성 Plan Table의 속성으로 변환 모든 일정 공유여부 상관없이 comment 출력 가능

--- src/main/java/com/example/VivaLaTrip/Entity/PublicPlan.java
+++ src/main/java/com/example/VivaLaTrip/Entity/PublicPlan.java
@@ -17,8 +17,6 @@ public class PublicPlan implements Serializable {
     @JoinColumn(name = "plan_id")
     private Plan plan;
 
-    @Column(name = "comment",nullable = false)
-    private String comment;
 
     @Column(name = "like_count")
     private int like_count;
