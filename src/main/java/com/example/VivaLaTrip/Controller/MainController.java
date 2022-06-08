cde1034ed9bae01941a2f00b6705735fe7523a1b
dbstjdgus <dbstj@192.168.219.100>
2022-06-09 02:18:14 +0900

    MainController 의존성 주입 삭제, 장소추가 메소드 수정(무한루프 방지)

--- src/main/java/com/example/VivaLaTrip/Controller/MainController.java
+++ src/main/java/com/example/VivaLaTrip/Controller/MainController.java
@@ -1,37 +1,12 @@
 package com.example.VivaLaTrip.Controller;
 
-import com.example.VivaLaTrip.Service.MapService;
-import com.example.VivaLaTrip.Service.PubTransService;
-import com.example.VivaLaTrip.Service.PublicPlanService;
-import com.example.VivaLaTrip.Service.UserService;
-import com.mashape.unirest.http.exceptions.UnirestException;
 import lombok.extern.slf4j.Slf4j;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.*;
 
 @Slf4j
 @Controller
 public class MainController {
-    private final UserService userService;
-    private final PublicPlanService publicPlanService;
-    private final PubTransService pubTransService;
-    private final MapService mapService;
-
-    @Autowired
-    public MainController(UserService userService, PublicPlanService publicPlanService, PubTransService pubTransService, MapService mapService) {
-        this.userService = userService;
-        this.publicPlanService = publicPlanService;
-        this.pubTransService = pubTransService;
-        this.mapService = mapService;
-    }
-
-    //인덱스 페이지 호출을 위한 Mapping
-    @GetMapping("/")
-    public String toIndex() {
-        return "index";
-    }
-
     @GetMapping("*")
     public String goIndex() {
         return "index";
--- src/main/java/com/example/VivaLaTrip/Service/MapService.java
+++ src/main/java/com/example/VivaLaTrip/Service/MapService.java
@@ -32,6 +32,99 @@ public class MapService {
         this.mapRepository = mapRepository;
     }
 
+    //14만개의 places데이터를 담고있는 데이터베이스를 참조하여 원하는 Places값을 뽑아오는 함수
+    public List<Places> MapParsingDB(String word) {
+
+        //사용자가 원하는 키워드가 포함된 모든 Places의 데이터를 뽑아옴(인기순으로 정렬)
+        List<Places> bodyJson = mapRepository.findByAddressNameContains(word, Sort.by(Sort.Order.desc("popularity")));
+
+        //Places의 개수를 100개 이하로 잘라서 리턴.
+        if (bodyJson.size() > 100) {
+            return bodyJson.subList(0, 100);
+        }
+        return bodyJson;
+    }
+
+    //사용자가 Plan을 만들 때 장소 개수에 비해 일정이 지나치게 짧아 의도한 대로 알고리즘이 생성되지 않을 때 자체적으로 장소를 추천하여 일정에 장소들을 추가해주는 함수
+    public List<Places> placeAdd(List<Places> places, double minPlace) {
+
+        int avgPopularity = 0;
+        double x_max = places.get(0).getX();
+        double x_min = places.get(0).getX();
+        double y_max = places.get(0).getY();
+        double y_min = places.get(0).getY();
+
+        for (Places p : places){
+            if (x_max < p.getX()){
+                x_max = p.getX();
+            }if (x_min > p.getX()){
+                x_min = p.getX();
+            }if (y_max < p.getY()){
+                y_max = p.getY();
+            }if (y_min > p.getY()){
+                y_min = p.getY();
+            }
+            avgPopularity += p.getPopularity();
+        }
+
+        avgPopularity = avgPopularity / places.size();
+        double distance = 0.005;  //500m
+        int index = 0;  //반복 횟수 - 범위를 점차 늘리기 위해
+
+        //사용자가 입력한 장소들의 500미터 범위에 있는 모든 Places를 뽑아옴 그래도 개수가 부족하다면 반경을 점점 500미터 씩 늘려서 장소를 뽑아옴
+        while (minPlace > places.size()){
+            List<Places> extraPlaces = mapRepository.findByXBetweenAndYBetween(
+                    (x_min - distance * index),
+                    (x_max + distance * index),
+                    (y_min - distance * index),
+                    (y_max + distance * index),
+                    Sort.by(Sort.Order.desc("popularity")));
+            //사용자로부터 입력받은 모든 Places들의 인기도 평균을 계산하여 인기도가 그 이상인 Places들만 솎아냄
+            for (Places extraPlace : extraPlaces){
+                if (!places.contains(extraPlace) && extraPlace.getPopularity() > avgPopularity - (avgPopularity / 10) * index) {
+                    places.add(extraPlace);
+                }
+            }
+            index++;
+        }
+
+        return places;      //추천된 장소들을 리턴
+    }
+
+    //경로 계산 알고리즘시 동작 되는 함수(중심 좌표 2키로 반경 숙소 값 파싱)
+    @JsonIgnoreProperties(ignoreUnknown = true)
+    @JsonInclude(JsonInclude.Include.NON_NULL)
+    @ResponseBody
+    public PlanDetailDTO getHotel(double x, double y) throws UnirestException, JsonProcessingException {
+
+        /*주요코드*/
+        //카카오에서 제공하는 API호출을 위한 Url로 KAKAO가 가지고 있는 숙소 값을을 뽑아옴 중심 x,y좌표 기준 2키로 반경
+        String APIKey = "KakaoAK ";
+        String apiURL ="https://dapi.kakao.com/v2/local/search/category.json?" +
+                "page=1&category_group_code=AD5&x="+x+"&y="+y+"&radius=20000&sort=distance";
+
+        HttpResponse<JsonNode> response = Unirest.get(apiURL)
+                .header("Authorization", APIKey).asJson();
+
+        ObjectMapper objectMapper =new ObjectMapper();
+        objectMapper.configure(DeserializationFeature.ACCEPT_SINGLE_VALUE_AS_ARRAY, true);
+
+        //숙소들에 담긴 x,y값 및 주소, 장소 이름 등이 실제적으로 담기는 객체 호출(KAKAO에서 제공하는 key값들에 맞추어 자체 제작)
+        KakaoGeoRes bodyJson = objectMapper.readValue(response.getBody().toString(), KakaoGeoRes.class);
+        List<Documents> result = new ArrayList<>(bodyJson.getDocuments());
+
+        PlanDetailDTO hotel = PlanDetailDTO.builder()
+                .id(result.get(0).getId())
+                .place_name(result.get(0).getPlace_name())
+                .x(Double.parseDouble(result.get(0).getX()))
+                .y(Double.parseDouble(result.get(0).getY()))
+                .build();
+
+        return hotel;   //15개의 숙소 값 리턴
+    }
+
+/*
+
     //자체 데이터 베이스 저장 전 Places 값들을 호출하기 위해 사용 됐던 KAKAO MAP의 명소 호출 함수(45개)
     @JsonIgnoreProperties(ignoreUnknown = true)
     @JsonInclude(JsonInclude.Include.NON_NULL)
@@ -40,7 +133,10 @@ public class MapService {
 
         String APIKey = "KakaoAK ";                         //kakao에서 제공하는 API KEY를 담는 변수
 
-        /*주요코드*/
+        */
+
+/*주요코드*//*
+
         //카카오에서 제공하는 API호출을 위한 Url로 KAKAO가 가지고 있는 명소 값을을 뽑아옴(키워드를 입력 받아야 가능 ..ex)서울 명소
         //한 번 요청에 최대 15개가 최대이므로 총 3번 요청함
         HashMap<String, Object> map1 = new HashMap<>();
@@ -87,99 +183,7 @@ public class MapService {
 
         return result;  //45개에 대한 장소정보 리턴
     }
-
-    //14만개의 places데이터를 담고있는 데이터베이스를 참조하여 원하는 Places값을 뽑아오는 함수
-    public List<Places> MapParsingDB(String word) {
-
-        //사용자가 원하는 키워드가 포함된 모든 Places의 데이터를 뽑아옴(인기순으로 정렬)
-        List<Places> bodyJson = mapRepository.findByAddressNameContains(word, Sort.by(Sort.Order.desc("popularity")));
-
-        //Places의 개수를 100개 이하로 잘라서 리턴.
-        if (bodyJson.size() > 100) {
-            return bodyJson.subList(0, 100);
-        }
-        return bodyJson;
-    }
-
-    //경로 계산 알고리즘시 동작 되는 함수(중심 좌표 2키로 반경 숙소 값 파싱)
-    @JsonIgnoreProperties(ignoreUnknown = true)
-    @JsonInclude(JsonInclude.Include.NON_NULL)
-    @ResponseBody
-    public PlanDetailDTO getHotel(double x, double y) throws UnirestException, JsonProcessingException {
-
-        /*주요코드*/
-        //카카오에서 제공하는 API호출을 위한 Url로 KAKAO가 가지고 있는 숙소 값을을 뽑아옴 중심 x,y좌표 기준 2키로 반경
-        String APIKey = "KakaoAK ";
-        String apiURL ="https://dapi.kakao.com/v2/local/search/category.json?" +
-                "page=1&category_group_code=AD5&x="+x+"&y="+y+"&radius=20000&sort=distance";
-
-        HttpResponse<JsonNode> response = Unirest.get(apiURL)
-                .header("Authorization", APIKey).asJson();
-
-        ObjectMapper objectMapper =new ObjectMapper();
-        objectMapper.configure(DeserializationFeature.ACCEPT_SINGLE_VALUE_AS_ARRAY, true);
-
-        //숙소들에 담긴 x,y값 및 주소, 장소 이름 등이 실제적으로 담기는 객체 호출(KAKAO에서 제공하는 key값들에 맞추어 자체 제작)
-        KakaoGeoRes bodyJson = objectMapper.readValue(response.getBody().toString(), KakaoGeoRes.class);
-        List<Documents> result = new ArrayList<>(bodyJson.getDocuments());
-
-        PlanDetailDTO hotel = PlanDetailDTO.builder()
-                .id(result.get(0).getId())
-                .place_name(result.get(0).getPlace_name())
-                .x(Double.parseDouble(result.get(0).getX()))
-                .y(Double.parseDouble(result.get(0).getY()))
-                .build();
-
-        return hotel;   //15개의 숙소 값 리턴
-    }
-
-    //사용자가 Plan을 만들 때 장소 개수에 비해 일정이 지나치게 짧아 의도한 대로 알고리즘이 생성되지 않을 때 자체적으로 장소를 추천하여 일정에 장소들을 추가해주는 함수
-    public List<Places> placeAdd(List<Places> places, double minPlace) {
-
-        int avgPopularity = 0;
-        double x_max = places.get(0).getX();
-        double x_min = places.get(0).getX();
-        double y_max = places.get(0).getY();
-        double y_min = places.get(0).getY();
-
-        for (Places p : places){
-            if (x_max < p.getX()){
-                x_max = p.getX();
-            }if (x_min > p.getX()){
-                x_min = p.getX();
-            }if (y_max < p.getY()){
-                y_max = p.getY();
-            }if (y_min > p.getY()){
-                y_min = p.getY();
-            }
-            avgPopularity += p.getPopularity();
-        }
-
-        avgPopularity = avgPopularity / places.size();
-        double distance = 0.005;  //500m
-        int index = 0;  //반복 횟수 - 범위를 점차 늘리기 위해
-
-        //사용자가 입력한 장소들의 500미터 범위에 있는 모든 Places를 뽑아옴 그래도 개수가 부족하다면 반경을 점점 500미터 씩 늘려서 장소를 뽑아옴
-        while (minPlace > places.size()){
-            List<Places> extraPlaces = mapRepository.findByXBetweenAndYBetween(
-                    (x_min - distance * index),
-                    (x_max + distance * index),
-                    (y_min - distance * index),
-                    (y_max + distance * index),
-                    Sort.by(Sort.Order.desc("popularity")));
-            //사용자로부터 입력받은 모든 Places들의 인기도 평균을 계산하여 인기도가 그 이상인 Places들만 솎아냄
-            for (Places extraPlace : extraPlaces){
-                if (!places.contains(extraPlace) && extraPlace.getPopularity() > avgPopularity){
-                    places.add(extraPlace);
-                }
-            }
-            index++;
-        }
-
-        return places;      //추천된 장소들을 리턴
-    }
-
-/*    public void Save_Places(KakaoGeoRes bodyJson[]) {
+    public void Save_Places(KakaoGeoRes bodyJson[]) {
 //        log.info("세이브 메소드에서 받은 bodyJson id값 : " + bodyJson[0].getDocuments().get(0).getId());
 
         Places[] places = new Places[15];
@@ -207,7 +211,10 @@ public class MapService {
 //        log.info("for문 적상 작동");
 //        log.info("맵 리포지토리 정상 작동");
     }
-
+*/
+    //자동화 코드 정사각형 1km^2으로 대한민국 행렬로 탐색 하여 카카오에서 제공하는 100퍼센트의 명소(카테고리) 값들을 뽑아오는
+    //코드
+/*7
     @JsonIgnoreProperties(ignoreUnknown = true)
     @JsonInclude(JsonInclude.Include.NON_NULL)
     @ResponseBody
@@ -216,7 +223,7 @@ public class MapService {
         최북단 : 강원도 고성군 현내면 대강리 *38.61503025970676 128.3580969588429
         최남단 : 제주특별자치도 서귀포시 대정읍 마라리 *33.11260291103729 126.26834665821991
         최동단 : 경상북도 울릉군 울릉읍 독도리 동도 37.23981774230282 *131.8727302252229945
-        최서단 : 인천광역시 옹진군 백령면 연화리. 37.96878377086497 *124.60976081654518
+        최서단 : 인천광역시 옹진군 백령면 연화리. 37.9687837086497 *124.60976081654518
         육지 최동단 : 포항 어딘가 위도 36.01670936347706, 경도 129.5845724459377
 
         최븍서(좌상단 꼭지점) : 38.61503025970676 124.60976081654518
